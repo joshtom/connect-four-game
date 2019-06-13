0d1b5d7b6507dd9a6a7b6d42375e6ce7acf453fa
Joshua <olajidejoshua4real@gmail.com>
2019-06-13 12:55:40 +0100

    Property added to Player class

--- js/Player.js
+++ js/Player.js
@@ -1,3 +1,13 @@
+ // The property below holds players information
+    //  name of the player - name
+    // color the player uses - color
+    // id of the player - id
+    // tokens is use to check the behavior of the player i.e moving up, down, left or right in the game
 class Player {
-    
+    constructor (name, color, id, tokens = false){
+    this.name = name;
+    this.color = color;
+    this.id = id;
+    this.tokens = [];
+}
 }

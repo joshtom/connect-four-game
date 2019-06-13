f5a7ec9dc218d35536247101e752980cd120ad52
Joshua <olajidejoshua4real@gmail.com>
2019-06-13 14:00:30 +0100

    Appropriate comment added

--- js/Player.js
+++ js/Player.js
@@ -4,16 +4,17 @@
     // id of the player - id
     // tokens is use to check the behavior of the player i.e moving up, down, left or right in the game
 class Player {
-    constructor (name, color, id, tokens = false){
+    constructor (name, color, id, active = false){
     this.name = name;
     this.color = color;
     this.id = id;
+    this.active = active;
     this.tokens = this.createTokens(21);
 }
 
 /**
  * Creates token objects for player
- * @param     {number}    num - Number of token objects to be created
+ * @param     {Integer}    num - Number of token objects to be created
  * @returns   {Array}     An array of the newly created token objects
  */
 createTokens(num) {

3e645fb2c2c0e15a91b4e7064debf1759e0f0d87
Joshua <olajidejoshua4real@gmail.com>
2019-06-13 13:47:27 +0100

    function that creates token objects for player added in the player class

--- js/Player.js
+++ js/Player.js
@@ -8,6 +8,22 @@ class Player {
     this.name = name;
     this.color = color;
     this.id = id;
-    this.tokens = [];
+    this.tokens = this.createTokens(21);
+}
+
+/**
+ * Creates token objects for player
+ * @param     {number}    num - Number of token objects to be created
+ * @returns   {Array}     An array of the newly created token objects
+ */
+createTokens(num) {
+    const tokens = [];
+
+    for(let i = 0; i < num; i++ ){
+        let token = new Token(i, this);
+        tokens.push(token);
+    }
+    return tokens;
 }
 }
+

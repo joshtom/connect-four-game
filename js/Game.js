084b8463d1b3c7bae30f995a352f3c5eeee626b1
Joshua <olajidejoshua4real@gmail.com>
2019-06-16 18:42:55 +0100

    Fixed Error made on the code that rendered the disc to sit horizontally

--- js/Game.js
+++ js/Game.js
@@ -91,20 +91,25 @@ class Game{
         // Checking if the Disc is aligned Vetically
         for( let x = 0; x < this.board.columns; x++ ) {
             for( let y = 0; y < this.board.rows - 3; y++ ) {
+                console.log(x,y);
+				console.log(y+1);
+				console.log(y+2);
+				console.log(y+3);
                 if(this.board.spaces[x][y].owner === owner &&
                     this.boaard.spaces[x][y+1].owner === owner &&
                     this.board.spaces[x][y+2].owner === owner &&
                     this.board.spaces[x][y+3].owner === owner){
                         win = true;
+                        console.log(win);
                 }
             }
         }
 
          // Checking if the Disc is aligned Horizontallu
-         for( let x = 0; x < this.board.columns; x++ ) {
-            for( let y = 0; y < this.board.rows - 3; y++ ) {
+         for( let x = 0; x < this.board.columns - 3; x++ ) {
+            for( let y = 0; y < this.board.rows; y++ ) {
                 if(this.board.spaces[x][y].owner === owner &&
-                    this.boaard.spaces[x+1][y].owner === owner &&
+                    this.board.spaces[x+1][y].owner === owner &&
                     this.board.spaces[x+2][y].owner === owner &&
                     this.board.spaces[x+3][y].owner === owner){
                         win = true;

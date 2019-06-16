2745c5576de942f85eef8db6cd8711259e86bd28
Joshua <olajidejoshua4real@gmail.com>
2019-06-16 03:03:39 +0100

    Another function added to update the board if any of the player has dropped the disc

--- js/Space.js
+++ js/Space.js
@@ -30,4 +30,12 @@ class Space {
     mark(token) {
         this.token = token;
     }
+
+    get owner() {
+        if(this.token === null){
+            return null;
+        } else {
+            return this.token.owner;
+        }
+    }
 }

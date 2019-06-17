3225fc5f2ce535bd7f5acbd9bdb70cda847fb8bb
Joshua <olajidejoshua4real@gmail.com>
2019-06-17 12:08:13 +0100

    Instruction page tracted and updated, Update local Repo"

---
+++ js/Instruction.js
@@ -0,0 +1,12 @@
+class Instruction{
+    alert() {
+       alert(`Playing This Game requires Two Players, The first and second player will be assigned to different DiscsRespectively \n 
+              You win by connecting four discs either Horizontally, Vertically, or Diagonally. \t GoodLuck Playing.
+       `);
+    }
+    
+        
+    
+}
+
+const instruction = new Instruction;
